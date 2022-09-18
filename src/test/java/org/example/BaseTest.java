74a12c7c9673c79244c83b0d8167cf93008ae8ed
Alex Sudarikov <galee.91@gmail.com>
2022-09-18 22:42:31 +0300

    SUD-1_Added PAgeObject

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,2 +1,35 @@
 package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUp() {
+        //Initialize web driver
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        //Set Up
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+//Create pages
+        loginPage = new LoginPage(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,1 +1,24 @@
 package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void userShouldLoginWithValidCredentials() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        assertTrue(driver.findElement(By.cssSelector(".title")).isDisplayed(), "User Wasn't Logged");
+    }
+    @Test
+    public void passwordShouldBeRequiredForLogin() {
+        loginPage.open();
+        loginPage.login("standard_user","");
+        assertEquals(loginPage.getError(),"Epic sadface: Password is required", "The error is incorrect");
+    }
+}

13bf6031b9e879eb571272499d8d360dcaf0aca6
Alex Sudarikov <galee.91@gmail.com>
2022-09-18 17:43:45 +0300

    Sudarikov_Update_Dependencies

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -1,12 +1,1 @@
 package org.example.pages;
-
-import org.openqa.selenium.WebDriver;
-
-public class BasePage {
-
-    WebDriver driver;
-
-    public BasePage(WebDriver driver) {
-        this.driver = driver;
-    }
-}
